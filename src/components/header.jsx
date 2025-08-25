8ea112f11f259c5b4b023be10f71dae27f80eac3
Nimesh Dilshan <nimeshdilshan869@gmail.com>
2025-08-25 21:23:00 +0530

    day 15🔥

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,18 @@
-export default function Header(){
-return(
-    <header className="w-full h-[100px] bg-accent text-white px-[40px]">
+import { Link } from "react-router-dom";
 
-        <div className="w-full h-full flex relative">
-            <img src="/public/logo.png" className="h-full w-[170px] left-0 object-cover absolute" />
-
-            <div className="h-full w-full flex justify-center items-center text-lg gap-[20px]">
-                <a href="/">Home</a>
-                <a href="/products">Products</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-            </div>
-
-        </div>
-    </header>
-)
+export default function Header() {
+	return (
+		<header className="w-full bg-accent  h-[100px] text-white px-[40px]">
+			<div className="w-full h-full flex relative ">
+				<img src="/logo.png" className="h-full absolute w-[170px] left-0  object-cover" />
+                <div className="h-full flex justify-center items-center w-full text-lg gap-[20px]">
+					
+                    <Link to="/">Home</Link>
+                    <Link to="/products">Products</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+			</div>
+		</header>
+	);
 }

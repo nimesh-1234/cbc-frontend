6cb69699fa87fe332143d1e70ab33f3e937d923c
Nimesh Dilshan <nimeshdilshan869@gmail.com>
2025-08-28 12:50:16 +0530

    day 15✏️

---
+++ src/pages/admin/adminAddNewProduct.jsx
@@ -0,0 +1,7 @@
+export default function AddProductPage() {
+	return (
+		<div className="w-full h-full">
+			sample add product page
+		</div>
+	);
+}
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,17 +1,47 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import { FaChartLine } from "react-icons/fa";
+import { MdShoppingCartCheckout } from "react-icons/md";
+import { BsBox2Heart } from "react-icons/bs";
+import { HiOutlineUsers } from "react-icons/hi";
+import AdminProductPage from "./admin/adminProductPage";
+import AddProductPage from "./admin/adminAddNewProduct";
 
 export default function AdminPage() {
   return (
-    <div className="w-full h-full bg-primary flex p-2">
-      <div className="w-[300px] h-full bg-primary">
-
+    <div className="w-full h-full bg-primary flex p-2 text-secondary">
+      <div className="w-[300px] h-full bg-primary flex flex-col items-center gap-[20px]">
+        <div className="flex flex-row w-[90%] h-[70px] bg-accent items-center rounded-2xl mb-[20px]">
+            <img src="/logo.png" alt="CBC"  className="h-[70px] "/>
+            <span className="text-white text-xl ml-">Admin Panel</span>
+        </div>
+        
+        <Link to="/admin" className="w-[90%] flex items-center gap-2 px-2 rounded-lg">
+            <FaChartLine/>
+            Dashboard
+        </Link>
+         <Link to="/admin/orders" className="w-[90%] flex items-center gap-2 px-2 rounded-lg">
+            <MdShoppingCartCheckout className="text-xl" />
+            Orders
+        </Link>
+         <Link to="/admin/products" className="w-[90%] flex items-center gap-2 px-2 rounded-lg">
+            <BsBox2Heart />
+            products
+        </Link>
+        <Link to="/admin/users" className="w-[90%] flex items-center gap-2 px-2 rounded-lg">
+            <HiOutlineUsers />
+            User
+        </Link>
       </div>
-      <div className="w-[calc(100%-300px)] h-full border-[2px] border-accent rounded-[20px]">
+      <div className="w-[calc(100%-300px)] h-full border-[4px] border-accent rounded-[20px] overflow-hidden">
+        <div className= "h-full w-full max-w-full  max-h-full overflow-y-scroll">
           <Routes path="/">
               <Route path="/" element={<h1>Admin Dashboard</h1>}/>
-              <Route path="products" element={<h1>products</h1>}/>
+              <Route path="products" element={<AdminProductPage/>}/>
               <Route path="orders" element={<h1>orders</h1>}/>
+              <Route path="add-product" element={<AddProductPage/>}/>
           </Routes>
+        </div>
+          
       </div>
     </div>
   );

2324f23871a771e5eaa8ba9736d5c4b3069264d9
Nimesh Dilshan <nimeshdilshan869@gmail.com>
2025-10-03 21:50:52 +0530

    ✏️

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,5 +1,5 @@
 import { CiCircleChevDown, CiCircleChevUp } from "react-icons/ci";
-import { addToCart, getTotal, loadCart, removeFromCart } from "../utils/cart";
+import { addToCart, getTotal, loadCart } from "../utils/cart";
 import { BiTrash } from "react-icons/bi";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -8,37 +8,37 @@ export default function CartPage() {
   const [cart, setCart] = useState(loadCart());
 
   return (
-    <div className="w-full h-[calc(100vh-100px)] bg-primary flex flex-col pt-[25px] items-center">
-      <div className="w-[600px] flex flex-col gap-4">
+    <div className="w-full lg:h-[calc(100vh-100px)] bg-primary flex flex-col pt-[25px] items-center">
+      <div className="w-[400px] lg:w-[600px] flex flex-col gap-4 ">
         {cart.map((item, index) => {
           return (
             <div
               key={index}
-              className="w-full h-[120px] bg-white flex relative items-center"
+              className="w-full h-[300px] lg:h-[120px] bg-white flex flex-col lg:flex-row relative items-center p-3 lg:p-0"
             >
               <button
                 className="absolute  text-red-500 right-[-40px] text-2xl rounded-full aspect-square hover:bg-red-500 hover:text-white p-[5px] font-bold"
                 onClick={() => {
-                  removeFromCart(item.productId);
+                  addToCart(item, -item.quantity);
                   setCart(loadCart());
                 }}
               >
                 <BiTrash />
               </button>
               <img
-                className="h-full aspect-square object-cover"
+                className="h-[100px] lg:h-full aspect-square object-cover"
                 src={item.image}
               />
-              <div className="w-[200px] h-full  flex flex-col pl-[5px] pt-[10px] ">
+              <div className="w-full text-center lg:w-[200px] h-[100px] lg:h-full flex flex-col pl-[5px] pt-[10px] ">
                 <h1 className=" font-semibold text-lg w-full text-wrap">
                   {item.name}
                 </h1>
-                {/* productID */}
+                {/* productId */}
                 <span className="text-sm text-secondary ">
                   {item.productId}
                 </span>
               </div>
-              <div className="w-[100px] h-full flex flex-col justify-center items-center ">
+              <div className="w-[100px] h-full flex flex-row lg:flex-col justify-center items-center ">
                 <CiCircleChevUp
                   className="text-3xl"
                   onClick={() => {
@@ -55,29 +55,29 @@ export default function CartPage() {
                   }}
                 />
               </div>
-              <div className="w-[180px]  h-full flex flex-col">
+              <div className="w-full lg:w-[180px] lg:h-full items-center justify-center  flex flex-row lg:flex-col">
                 {item.labelledPrice > item.price && (
-                  <span className="text-secondary w-full  text-right line-through text-lg pr-[10px] mt-[20px]">
+                  <span className="text-secondary lg:w-full   text-center  lg:text-right line-through text-lg pr-[10px] lg:mt-[20px]">
                     LKR {item.labelledPrice.toFixed(2)}
                   </span>
                 )}
-                <span className="font-semibold text-accent w-full text-right text-2xl pr-[10px] mt-[5px]">
+                <span className="font-semibold text-accent  lg:w-full text-center  lg:text-right text-2xl pr-[10px] lg:mt-[5px]">
                   LKR {item.price.toFixed(2)}
                 </span>
               </div>
             </div>
           );
         })}
-        <div className="w-full h-[120px] bg-white flex justify-end items-center relative">
+        <div className="w-full lg:w-full h-[120px] bg-white flex flex-col-reverse  lg:flex-row justify-end items-center relative">
           <Link
             state={cart}
             to="/checkout"
-            className="absolute left-0 bg-accent text-white px-6 py-3  ml-[20px] hover:bg-accent/80"
+            className="lg:absolute left-0 bg-accent text-white px-6 py-3  lg:ml-[20px] hover:bg-accent/80"
           >
             Proceed to Checkout
           </Link>
           <div className="h-[50px]">
-            <span className="font-semibold text-accent w-full text-right text-2xl pr-[10px] mt-[5px]">
+            <span className="font-semibold text-accent lg:w-full text-center   lg:text-right text-2xl p-0 lg:pr-[10px] mt-[5px]">
               Total: LKR {getTotal().toFixed(2)}
             </span>
           </div>

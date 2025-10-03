75357d93c9889edfb21c6dd454a367a2f72a1a01
Nimesh Dilshan <nimeshdilshan869@gmail.com>
2025-10-03 21:25:22 +0530

    product-overview mobile responsive✏️

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,11 +1,5 @@
-
-
 export default function TestPage() {
-	
-	return(
-		<div className="w-[400px] h-[400px] bg-green-200 relative">
-			<div className="w-[200px] h-[200px] bg-red-400 z-[-50] absolute top-0 left-0 right-0 bottom-0 m-auto"></div>
-			<div className="w-[200px] h-[200px] bg-blue-400 z-[0] absolute top-0 left-0 right-0 bottom-0 m-auto"></div>
-		</div>
-	)
+  return (
+    <div className="w-[400px] h-[400px] bg-green-600 lg:bg-blue-600 relative"></div>
+  );
 }
